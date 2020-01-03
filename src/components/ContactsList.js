87db52ef695145466e0ec2cd746055c0e842c5ea
Aman <amankumar.edith@gmail.com>
2020-01-03 14:45:53 +0530

    Added basic UI for contact list

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -3,9 +3,21 @@ import PropTypes from 'prop-types'
 
 const ContactsList = ({ contacts }) => {
   return (
-    <ol>
+    <ol className='contact-list'>
       {contacts.map(contact => (
-        <li key={contact.id}>{contact.name}</li>
+        <li key={contact.id} className='contact-list-item'>
+          <div
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`,
+            }}
+            className='contact-avatar'
+          ></div>
+          <div className='contact-details'>
+            <p>{contact.name}</p>
+            <p>{contact.avatarURL}</p>
+          </div>
+          <button className='contact-remove'>Remove</button>
+        </li>
       ))}
     </ol>
   )
