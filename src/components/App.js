ca782971aa4a8230334590bfd6679e5fa393fa9e
Aman <amankumar.edith@gmail.com>
2020-01-03 15:56:47 +0530

    Moved contacts to state of App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 import ContactsList from './ContactsList'
 
-const contacts = [
-  {
-    id: 'karen',
-    name: 'Karen Isgrigg',
-    handle: 'karen_isgrigg',
-    avatarURL: 'http://localhost:5001/karen.jpg',
-  },
-  {
-    id: 'richard',
-    name: 'Richard Kalehoff',
-    handle: 'richardkalehoff',
-    avatarURL: 'http://localhost:5001/richard.jpg',
-  },
-  {
-    id: 'tyler',
-    name: 'Tyler McGinnis',
-    handle: 'tylermcginnis',
-    avatarURL: 'http://localhost:5001/tyler.jpg',
-  },
-]
+class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: 'karen_isgrigg',
+        avatarURL: 'http://localhost:5001/karen.jpg',
+      },
+      {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: 'richardkalehoff',
+        avatarURL: 'http://localhost:5001/richard.jpg',
+      },
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: 'tylermcginnis',
+        avatarURL: 'http://localhost:5001/tyler.jpg',
+      },
+    ],
+  }
 
-const App = () => <ContactsList contacts={contacts} />
+  render() {
+    return <ContactsList contacts={this.state.contacts} />
+  }
+}
 
 export default hot(module)(App)
