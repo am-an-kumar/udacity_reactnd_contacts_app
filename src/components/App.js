90792405d570156509e2fcf04630541de90ea592
Aman <amankumar.edith@gmail.com>
2020-01-05 04:01:06 +0530

    Added AJAX code to fetch contacts from server

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 import ContactsList from './ContactsList'
+import * as ContactsAPI from '../utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        id: 'karen',
-        name: 'Karen Isgrigg',
-        handle: 'karen_isgrigg',
-        avatarURL: 'http://localhost:5001/karen.jpg',
-      },
-      {
-        id: 'richard',
-        name: 'Richard Kalehoff',
-        handle: 'richardkalehoff',
-        avatarURL: 'http://localhost:5001/richard.jpg',
-      },
-      {
-        id: 'tyler',
-        name: 'Tyler McGinnis',
-        handle: 'tylermcginnis',
-        avatarURL: 'http://localhost:5001/tyler.jpg',
-      },
-    ],
+    contacts: [],
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState({
+        contacts,
+      })
+    })
   }
 
   removeContact = contactToRemove => {
